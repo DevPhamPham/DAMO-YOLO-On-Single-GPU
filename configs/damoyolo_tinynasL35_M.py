e30bf69a682b9f14334432d27b7a54e09250aec1
DevPhamPham <pham.duykhoa1303@gmail.com>
2025-07-09 01:44:02 +0700

    update tinynasL35_M

--- configs/damoyolo_tinynasL35_M.py
+++ configs/damoyolo_tinynasL35_M.py
@@ -10,10 +10,10 @@ class Config(MyConfig):
         self.miscs.exp_name = os.path.splitext(os.path.basename(__file__))[0]
         self.miscs.eval_interval_epochs = 5
         self.miscs.ckpt_interval_epochs = 5
-        self.miscs.total_epochs = 150  # Tổng số epoch giảm xuống 150
 
         # 2) Lịch train
         self.train.batch_size = 10
+        self.train.total_epochs = 150
         self.train.base_lr_per_img = 0.01 / 64
         self.train.min_lr_ratio = 0.05
         self.train.weight_decay = 5e-4
